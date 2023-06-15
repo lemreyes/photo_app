0d2ffa46475f59429b184d9eb7425d239407bee4
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-06-15 21:06:50 +0800

    initial setup

---
+++ src/app/ImageUploader/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function ImageUploader() {
+  return (
+    <div className="flex items-center justify-center drop-shadow-md ">
+      <h1 className="text-center">Upload your image</h1>
+      <p className="text-center">File should be jpeg, png...</p>
+      <div>
+        
+      </div>
+    </div>
+  );
+}
