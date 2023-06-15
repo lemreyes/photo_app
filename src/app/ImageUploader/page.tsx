9e31f1dec890b32998429d289b5359e531f6eb7e
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-06-15 21:38:34 +0800

    image uploader frontend div

--- src/app/ImageUploader/page.tsx
+++ src/app/ImageUploader/page.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function ImageUploader() {
   return (
-    <div className="flex items-center justify-center drop-shadow-md ">
-      <h1 className="text-center">Upload your image</h1>
-      <p className="text-center">File should be jpeg, png...</p>
-      <div>
-        
+    <div className="flex flex-col items-center justify-center w-screen h-screen">
+      <div className="shadow-lg rounded-[12px] px-32 py-124">
+        <h1 className="text-center">Upload your image</h1>
+        <p className="text-center">File should be jpeg, png...</p>
+        <div className="border-2 border-[#97BEF4] rounded-[12px] border-dashed p-10"></div>
       </div>
     </div>
   );
