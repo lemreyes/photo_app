a1131aef1634f505bd5b9e337fc8464d7227af09
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-06-17 15:56:02 +0800

    Correct color of progress bar component

--- src/app/ImageUploader/Components/ProgressBar.tsx
+++ src/app/ImageUploader/Components/ProgressBar.tsx
@@ -4,10 +4,10 @@ import React from "react";
 export default function ProgressBar() {
   return (
     <div className="flex flex-col items-start justify-center shadow-lg rounded-[12px] min-w-[400px] px-8 py-10">
-      <h1 className="text-left text-lg text-gray-700">Uploading...</h1>
-      <div className="mt-8 min-w-[340px] h-1 bg-gray-300">
+      <h1 className="text-left text-lg text-[#4F4F4F]">Uploading...</h1>
+      <div className="mt-8 min-w-[340px] h-1 bg-[#F2F2F2]">
         <motion.div
-          className="h-1 max-w-[100px] bg-green-700"
+          className="h-1 max-w-[100px] bg-[#2F80ED]"
           animate={{ x: [0, 240, 0] }}
           transition={{ repeat: Infinity, duration: 2 }}
         />
