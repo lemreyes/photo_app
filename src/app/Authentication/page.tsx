3932e8dca674fed870a5f05b93b8c4bacd9427d7
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-06-20 21:56:17 +0800

    Signup form HTML structure

---
+++ src/app/Authentication/page.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Authentication() {
+  return (
+    <div className="flex flex-col items-center justify-center w-screen h-screen">
+      <div className="flex flex-col items-center justify-center shadow-lg rounded-xl px-8 py-10">
+        <h1>Logo</h1>
+        <p>Join thousands of learners from around the world </p>
+        <p>
+          Master web development by making real-life projects. There are
+          multiple paths for you to choose
+        </p>
+        <form method="POST">
+          <input type="text" placeholder="Email"></input>
+          <input type="password" placeholder="Password"></input>
+          <button>Start coding now</button>
+        </form>
+        <p>or continue with these social profiles</p>
+      </div>
+    </div>
+  );
+}
