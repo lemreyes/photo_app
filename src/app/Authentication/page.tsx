eb9dad5dd5827b1f241d4b140008da256e942dcf
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-06-21 21:43:47 +0800

    Authentication sign in front end

--- src/app/Authentication/page.tsx
+++ src/app/Authentication/page.tsx
@@ -9,19 +9,29 @@ export default function Authentication() {
     <div
       className={`${noto_sans.className} flex flex-col items-center justify-center w-screen h-screen`}
     >
-      <div className="flex flex-col items-center justify-center shadow-lg rounded-xl px-8 py-10">
+      <div className="flex flex-col items-start justify-center shadow-lg rounded-xl px-8 py-10  max-w-[473px]">
         <Logo />
-        <p>Join thousands of learners from around the world </p>
-        <p>
+        <p className="mt-8 text-lg font-semibold text-[#333333]">
+          Join thousands of learners from around the world{" "}
+        </p>
+        <p className="mt-4 text-base font-normal text-[#333333]">
           Master web development by making real-life projects. There are
           multiple paths for you to choose
         </p>
         <form method="POST">
-          <input type="text" placeholder="Email"></input>
-          <input type="password" placeholder="Password"></input>
-          <button>Start coding now</button>
+          <input
+            type="text"
+            placeholder="Email"
+            className="w-full border rounded-lg border-color-[#BDBDBD] text-[#828282] mt-8 p-2"
+          ></input>
+          <input
+            type="password"
+            placeholder="Password"
+            className="w-full border rounded-lg border-color-[#BDBDBD] text-[#828282] mt-2 p-2"
+          ></input>
+          <button className="w-full bg-[#2F80ED] text-white rounded-lg mt-4 p-2">Start coding now</button>
         </form>
-        <p>or continue with these social profiles</p>
+        <p className="mt-4 text-sm font-normal text-center">or continue with these social profiles</p>
       </div>
     </div>
   );
