ec63576522fbd439fdffd3cc171857b9d13ee992
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-06-21 21:09:36 +0800

    Add specific font for authentication page

--- src/app/Authentication/page.tsx
+++ src/app/Authentication/page.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { Noto_Sans } from "next/font/google";
+
+const noto_sans = Noto_Sans({ weight: ["400", "600"], subsets: ["latin"] });
 
 export default function Authentication() {
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen">
+    <div
+      className={`${noto_sans.className} flex flex-col items-center justify-center w-screen h-screen`}
+    >
       <div className="flex flex-col items-center justify-center shadow-lg rounded-xl px-8 py-10">
         <h1>Logo</h1>
         <p>Join thousands of learners from around the world </p>
