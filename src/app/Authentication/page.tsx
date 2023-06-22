1844da67cd616105d877487a3ae7805fb82a4956
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-06-22 23:47:40 +0800

    Signup page frontend

--- src/app/Authentication/page.tsx
+++ src/app/Authentication/page.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { Noto_Sans } from "next/font/google";
 import Logo from "./Components/Logo";
+import Image from "next/image";
+import google_icon from "../../../public/authentication/Google.svg";
+import facebook_icon from "../../../public/authentication/Facebook.svg";
+import twitter_icon from "../../../public/authentication/Twitter.svg";
+import github_icon from "../../../public/authentication/Gihub.svg";
+import Link from "next/link";
 
 const noto_sans = Noto_Sans({ weight: ["400", "600"], subsets: ["latin"] });
 
@@ -29,9 +35,22 @@ export default function Authentication() {
             placeholder="Password"
             className="w-full border rounded-lg border-color-[#BDBDBD] text-[#828282] mt-2 p-2"
           ></input>
-          <button className="w-full bg-[#2F80ED] text-white rounded-lg mt-4 p-2">Start coding now</button>
+          <button className="w-full bg-[#2F80ED] text-white rounded-lg mt-4 p-2">
+            Start coding now
+          </button>
         </form>
-        <p className="mt-4 text-sm font-normal text-center">or continue with these social profiles</p>
+        <p className="mt-8 text-sm text-[#828282] font-normal self-center">
+          or continue with these social profiles
+        </p>
+        <div className="flex mt-4 self-center space-x-4">
+          <Image src={google_icon} alt="Google" />
+          <Image src={facebook_icon} alt="Facebook" />
+          <Image src={twitter_icon} alt="Twitter" />
+          <Image src={github_icon} alt="Github" />
+        </div>
+        <p className="mt-8 text-sm text-[#828282] font-normal self-center">
+          Already a member? <Link href="Authentication/Login" className="text-[#2D9CDB]">Login</Link>
+        </p>
       </div>
     </div>
   );
