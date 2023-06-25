b29d4355e50d501926a656f7d174cca5a5492090
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-06-25 12:54:25 +0800

    Email and Password icons placeholders

--- src/app/Authentication/Components/CredentialsForm.tsx
+++ src/app/Authentication/Components/CredentialsForm.tsx
@@ -1,19 +1,38 @@
 import React from "react";
+import Image from "next/image";
+
 import email_icon from "../../../../public/authentication/email_icon.svg";
+import password_icon from "../../../../public/authentication/password_icon.svg";
 
 export default function CredentialsForm() {
   return (
     <form method="POST">
-      <input
-        type="text"
-        placeholder="Email"
-        className="w-full border rounded-lg border-color-[#BDBDBD] text-[#828282] mt-8 p-2"
-      ></input>
-      <input
-        type="password"
-        placeholder="Password"
-        className="w-full border rounded-lg border-color-[#BDBDBD] text-[#828282] mt-2 p-2"
-      ></input>
+      <label htmlFor="email" className="relative">
+        <Image
+          src={email_icon}
+          alt="email"
+          className="pointer-events-none w-6 h-6 absolute left-2 translate-y-10"
+        />
+        <input
+          id="email"
+          type="text"
+          placeholder="Email"
+          className="w-full border rounded-lg border-color-[#BDBDBD] text-[#828282] mt-8 p-2 pl-12"
+        />
+      </label>
+      <label htmlFor="password" className="relative">
+        <Image
+          src={password_icon}
+          alt="password"
+          className="pointer-events-none max-w-none w-6 h-6 absolute translate-y-4 translate-x-2"
+        />
+        <input
+          id="password"
+          type="password"
+          placeholder="Password"
+          className="w-full border rounded-lg border-color-[#BDBDBD] text-[#828282] mt-2 p-2 pl-12"
+        ></input>
+      </label>
       <button className="w-full bg-[#2F80ED] text-white rounded-lg mt-4 p-2">
         Login
       </button>
