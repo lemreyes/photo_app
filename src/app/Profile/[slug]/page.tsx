902236e06d6d685d45832dd837efd8b250e05645
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-06-25 20:04:51 +0800

    Profile table start

--- src/app/Profile/[slug]/page.tsx
+++ src/app/Profile/[slug]/page.tsx
@@ -2,12 +2,35 @@ import React from "react";
 import ProfileHeader from "./Components/ProfileHeader";
 
 import { Noto_Sans } from "next/font/google";
-const noto_sans = Noto_Sans({ weight: ["400", "600", "700"], subsets: ["latin"] });
+const noto_sans = Noto_Sans({
+  weight: ["300", "400", "600", "700"],
+  subsets: ["latin"],
+});
 
 export default function Profile() {
   return (
-    <div className={`${noto_sans.className} px-18 flex flex-col`}>
+    <>
       <ProfileHeader />
-    </div>
+      <div
+        className={`${noto_sans.className} px-18 flex flex-col items-center`}
+      >
+        <h1 className="text-3xl font-normal mt-12 tracking-tighter">
+          Personal info
+        </h1>
+        <h2 className="text-lg font-light tracking-tighter">
+          Basic info, like your name and photo
+        </h2>
+        <div className="border border-[#E0E0E0] rounded-xl mt-8">
+          <div className="py-8 px-16 flex flex-row">
+            <div>
+              <h2 className="text-2xl tracking-tighter">Profile</h2>
+              <p className="text-xs text-[#828282] font-medium tracking-tighter">Some info may be visible to other people</p>
+            </div>
+            <button className="border rounded-lg tracking-tighter text-[#828282] px-8 py-1 text-sm">Edit</button>
+          </div>
+          <div>Photo</div>
+        </div>
+      </div>
+    </>
   );
 }
