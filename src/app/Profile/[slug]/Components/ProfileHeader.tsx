049cb7e21cf60158146a2664f891fab62089decc
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-06-25 13:14:27 +0800

    Profile page and header start

---
+++ src/app/Profile/[slug]/Components/ProfileHeader.tsx
@@ -0,0 +1,10 @@
+import Logo from "@/app/Authentication/Components/Logo";
+import React from "react";
+
+export default function ProfileHeader() {
+    return (
+        <header className="flex justify-between">
+            <Logo />
+        </header>
+    )
+}
---
+++ src/app/Profile/[slug]/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ProfileHeader from "./Components/ProfileHeader";
+
+export default function Profile() {
+  return (
+    <div className="px-18">
+      <ProfileHeader />
+    </div>
+  );
+}
