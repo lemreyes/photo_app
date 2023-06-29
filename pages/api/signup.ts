6cf5c307bfa97a7dd15e4d6b3d7b27512ceda9b4
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-06-29 21:46:02 +0800

    Signup param validation

--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -1,8 +1,36 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import validator from "validator";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const { email, password } = JSON.parse(req.body);
+  const errors: string[] = [];
+
   console.log(req.body);
   console.log(email, password);
+
+  // validate
+  const validationSchema = [
+    {
+      valid: validator.isEmail(email),
+      errorMessage: "Email is invalid",
+    },
+    {
+      valid: validator.isStrongPassword(password),
+      errorMessage: "Password is not strong enough",
+    },
+  ];
+
+  validationSchema.forEach((check) => {
+    if (!check.valid) {
+      errors.push(check.errorMessage);
+    }
+  });
+
+  if (errors.length > 0) {
+    return res.status(400).json({
+      errorMessage: errors[0],
+    });
+  }
+
   return res.status(200).json("Test");
 }
