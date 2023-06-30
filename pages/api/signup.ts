6ef124b4af276a735486b89daa74c5e01ea9cb5c
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-06-30 11:35:37 +0800

    Signup backend implementation

--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -1,8 +1,17 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import validator from "validator";
+import bcrypt from "bcrypt";
+import * as jose from "jose";
+import { setCookie } from "cookies-next";
+import { PrismaClient } from "@prisma/client";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { email, password } = JSON.parse(req.body);
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { email, password } = req.body;
   let errors: string[] = [];
 
   console.log(req.body);
@@ -17,7 +26,43 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     });
   }
 
-  return res.status(200).json("Test");
+  // check if already existing email
+  const userEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+  if (userEmail) {
+    return res
+      .status(400)
+      .json({ errorMessage: "Email is associated with another account." });
+  }
+
+  // hash password
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  // data valid - store to DB
+  const user = await prisma.user.create({
+    data: {
+      email: email,
+      password: hashedPassword,
+    },
+  });
+  console.log(user);
+
+  const alg = "HS256";
+  const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+
+  const token = await new jose.SignJWT({ email: user.email })
+    .setProtectedHeader({ alg })
+    .setExpirationTime("24h")
+    .sign(secret);
+
+  setCookie("jwt", token, { req, res, maxAge: 60 * 6 * 24 });
+
+  res.status(200).json({
+    email: user.email,
+  });
 }
 
 function validateInputParam(email: string, password: string) {
