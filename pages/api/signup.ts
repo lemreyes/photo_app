00ccd714a7c0e18af6d61bf2e109426a1ae845f9
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-06-30 09:55:16 +0800

    signup validation refactor

--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -3,12 +3,26 @@ import validator from "validator";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const { email, password } = JSON.parse(req.body);
-  const errors: string[] = [];
+  let errors: string[] = [];
 
   console.log(req.body);
   console.log(email, password);
 
   // validate
+  errors = validateInputParam(email, password);
+  console.log(errors);
+  if (errors.length > 0) {
+    return res.status(400).json({
+      errorMessage: errors[0],
+    });
+  }
+
+  return res.status(200).json("Test");
+}
+
+function validateInputParam(email: string, password: string) {
+  let errors: string[] = [];
+
   const validationSchema = [
     {
       valid: validator.isEmail(email),
@@ -26,11 +40,5 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     }
   });
 
-  if (errors.length > 0) {
-    return res.status(400).json({
-      errorMessage: errors[0],
-    });
-  }
-
-  return res.status(200).json("Test");
+  return errors;
 }
