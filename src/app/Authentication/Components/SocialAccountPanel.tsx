0164ec0fe5275409712d5db051cd809a7d4886ab
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-07-10 19:27:36 +0800

    Nextauth Google signin

--- src/app/Authentication/Components/SocialAccountPanel.tsx
+++ src/app/Authentication/Components/SocialAccountPanel.tsx
@@ -1,18 +1,28 @@
 import React from "react";
 import Image from "next/image";
+
+import { useSession, signIn, signOut } from "next-auth/react";
+
 import google_icon from "../../../../public/authentication/Google.svg";
 import facebook_icon from "../../../../public/authentication/Facebook.svg";
 import twitter_icon from "../../../../public/authentication/Twitter.svg";
 import github_icon from "../../../../public/authentication/Gihub.svg";
 
 export default function SocialAccountPanel() {
+  const hdlOnClickGoogle = async () => {
+    const response = await signIn("google");
+    console.log("Sign in Response: ", response);
+  };
+
   return (
     <>
       <p className="mt-8 text-sm text-[#828282] font-normal self-center">
         or continue with these social profiles
       </p>
       <div className="flex mt-4 self-center space-x-4">
-        <Image src={google_icon} alt="Google" />
+        <button onClick={hdlOnClickGoogle}>
+          <Image src={google_icon} alt="Google" />
+        </button>
         <Image src={facebook_icon} alt="Facebook" />
         <Image src={twitter_icon} alt="Twitter" />
         <Image src={github_icon} alt="Github" />
--- src/app/Authentication/page.tsx
+++ src/app/Authentication/page.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import React from "react";
 import { Noto_Sans } from "next/font/google";
 import Logo from "./Components/Logo";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 import Link from "next/link";
 import SocialAccountPanel from "./Components/SocialAccountPanel";
@@ -9,28 +12,43 @@ import CredentialsForm from "./Components/CredentialsForm";
 const noto_sans = Noto_Sans({ weight: ["400", "600"], subsets: ["latin"] });
 
 export default function Authentication() {
+  const { data: session } = useSession();
+  console.log(session);
+
+  const hdlSignOut = async () => {
+    const response = await signOut();
+    console.log("Sign out respone: ", response);
+  };
+
   return (
     <div
       className={`${noto_sans.className} flex flex-col items-center justify-center w-screen h-screen`}
     >
-      <div className="flex flex-col items-start justify-center shadow-lg rounded-xl px-16 py-10  max-w-[473px]">
-        <Logo />
-        <p className="mt-8 text-lg font-semibold text-[#333333]">
-          Join thousands of learners from around the world{" "}
-        </p>
-        <p className="mt-4 text-base font-normal text-[#333333]">
-          Master web development by making real-life projects. There are
-          multiple paths for you to choose
-        </p>
-        <CredentialsForm />
-        <SocialAccountPanel />
-        <p className="mt-8 text-sm text-[#828282] font-normal self-center">
-          Already a member?{" "}
-          <Link href="Authentication/Login" className="text-[#2D9CDB]">
-            Login
-          </Link>
-        </p>
-      </div>
+      {session ? (
+        <div>
+          <p>Already logged in as {session?.user?.email}.</p>
+          <button onClick={hdlSignOut}>Sign Out</button>
+        </div>
+      ) : (
+        <div className="flex flex-col items-start justify-center shadow-lg rounded-xl px-16 py-10  max-w-[473px]">
+          <Logo />
+          <p className="mt-8 text-lg font-semibold text-[#333333]">
+            Join thousands of learners from around the world{" "}
+          </p>
+          <p className="mt-4 text-base font-normal text-[#333333]">
+            Master web development by making real-life projects. There are
+            multiple paths for you to choose
+          </p>
+          <CredentialsForm isLogin={false} />
+          <SocialAccountPanel />
+          <p className="mt-8 text-sm text-[#828282] font-normal self-center">
+            Already a member?{" "}
+            <Link href="Authentication/Login" className="text-[#2D9CDB]">
+              Login
+            </Link>
+          </p>
+        </div>
+      )}
     </div>
   );
 }
