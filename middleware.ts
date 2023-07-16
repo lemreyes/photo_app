0d9afd92d71c8530afb47f133d0f5eb33134b135
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-07-16 16:12:33 +0800

    Update middleware for nextauth

--- middleware.ts
+++ middleware.ts
@@ -1,48 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
-import * as jose from "jose";
-
-export async function middleware(req: NextRequest, res: NextResponse) {
-  console.log("MIDDLEWARE");
-
-  // get bearer token from authorization header
-  const bearerToken = req.headers.get("authorization") as string;
-  console.log(bearerToken);
-  if (!bearerToken) {
-    return new NextResponse(
-      JSON.stringify({
-        errorMessage: "Unauthorized request",
-      }),
-      { status: 401 }
-    );
-  }
-
-  // extract token
-  const token = bearerToken.split(" ")[1];
-  console.log(token);
-
-  if (!token) {
-    return new NextResponse(
-      JSON.stringify({
-        errorMessage: "Unauthorized request",
-      }),
-      { status: 401 }
-    );
-  }
-
-  // verify JWT
-  const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-
-  try {
-    await jose.jwtVerify(token, secret);
-  } catch (error) {
-    return new NextResponse(
-      JSON.stringify({
-        errorMessage: "Unauthorized request",
-      }),
-      { status: 401 }
-    );
-  }
-}
+export { default } from "next-auth/middleware"
 
 export const config = {
   matcher: ["/api/updateProfile", "/ImageUploader"],
